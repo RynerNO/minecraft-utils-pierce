077aa55a6749c3743c5a4a53522f2dc808c4d301
RynerNO <ryner.no@gmail.com>
2024-10-13 15:29:10 +0200

    remove cjs

--- build/cjs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.onLog = exports.offline = exports.Client = void 0;
-var launcher_1 = require("./launcher");
-Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return launcher_1.Client; } });
-var authenticator_1 = require("./authenticator");
-Object.defineProperty(exports, "offline", { enumerable: true, get: function () { return authenticator_1.offline; } });
-var log_1 = require("./utils/log");
-Object.defineProperty(exports, "onLog", { enumerable: true, get: function () { return log_1.onLog; } });
--- build/cjs/mclc.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Should be changed each update
-exports.default = '3.19.0';
